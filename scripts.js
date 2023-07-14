c270279d68975a38519c425775c9af9b9e7d139b
Gabe Breternitz <gabrielbreternitz@gmail.com>
2023-07-13 19:44:32 -0500

    added some JS elements to takeoff and flightstatus

--- scripts.js
+++ scripts.js
@@ -1,2 +1,20 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+window.addEventListener("load", function() {
+    
+    // Declare objects  up here
+    const takeoff = document.getElementById("takeoff");
+    const flightStatus = document.getElementById("flightStatus");
+
+    console.log(takeoff.innerHTML);
+
+    //TODO: Take off button stuff
+
+    takeoff.addEventListener("click", function () {
+        window.confirm("Confirm that the shuttle is ready for takeoff.")
+
+        flightStatus.innerHTML = "Shuttle in flight."
+    });
+
+});
