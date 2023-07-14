1d8e9703b59fffc520cbc2b05fb4246553cdedb5
Gabe Breternitz <gabrielbreternitz@gmail.com>
2023-07-13 20:05:25 -0500

    updated shuttleBackground and spaceShuttleHeight JS scripts

--- scripts.js
+++ scripts.js
@@ -6,15 +6,21 @@ window.addEventListener("load", function() {
     // Declare objects  up here
     const takeoff = document.getElementById("takeoff");
     const flightStatus = document.getElementById("flightStatus");
+    let shuttleBackground = document.getElementById("shuttleBackground");
+    let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
 
     console.log(takeoff.innerHTML);
 
     //TODO: Take off button stuff
 
     takeoff.addEventListener("click", function () {
-        window.confirm("Confirm that the shuttle is ready for takeoff.")
+        window.confirm("Confirm that the shuttle is ready for takeoff.");
 
-        flightStatus.innerHTML = "Shuttle in flight."
+        flightStatus.innerHTML = "Shuttle in flight.";
+        shuttleBackground.style.backgroundColor = 'blue';
+        spaceShuttleHeight.innerHTML = '10,000';
+
+
     });
 
 });
