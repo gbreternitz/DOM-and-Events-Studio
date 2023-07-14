1685d909ebfe2c1e27f74730bedcb7eba12c49fb
Gabe Breternitz <gabrielbreternitz@gmail.com>
2023-07-13 20:11:53 -0500

    added JS script to Land button

--- scripts.js
+++ scripts.js
@@ -5,11 +5,11 @@ window.addEventListener("load", function() {
     
     // Declare objects  up here
     const takeoff = document.getElementById("takeoff");
-    const flightStatus = document.getElementById("flightStatus");
+    const landing = document.getElementById("landing");
+
+    let flightStatus = document.getElementById("flightStatus");
     let shuttleBackground = document.getElementById("shuttleBackground");
     let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
-
-    console.log(takeoff.innerHTML);
 
     //TODO: Take off button stuff
 
@@ -19,8 +19,15 @@ window.addEventListener("load", function() {
         flightStatus.innerHTML = "Shuttle in flight.";
         shuttleBackground.style.backgroundColor = 'blue';
         spaceShuttleHeight.innerHTML = '10,000';
+    });
 
+    landing.addEventListener("click", function () {
+        window.alert("The shuttle is landing. Landing gear engaged.");
+        flightStatus.innerHTML = "The shuttle has landed.";
+        shuttleBackground.style.backgroundColor = 'green';
+        spaceShuttleHeight.innerHTML = '0';
 
     });
+
 
 });
