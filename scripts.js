79954fa9039f01be3ecb3e883d1bc4bf74ceff91
Gabe Breternitz <gabrielbreternitz@gmail.com>
2023-07-15 20:48:41 -0500

    completed all but bonus mission 1

--- scripts.js
+++ scripts.js
@@ -17,10 +17,14 @@ window.addEventListener("load", function() {
     let flightStatus = document.getElementById("flightStatus");
     let shuttleBackground = document.getElementById("shuttleBackground");
     let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+    let rocketInFlight = false;
 
-    rocket.style.bottom = "0px";
     rocket.style.position = "absolute";
+    rocket.style.bottom = "0px";
     rocket.style.left = "0px";
+    rocket.style.boxSizing = "border-box"; //doesn't work
+    rocket.style.maxHeight = "inherit"; // doesn't work
+
 
     takeoff.addEventListener("click", function () {
         takeoffFlag = window.confirm("Confirm that the shuttle is ready for takeoff."); // come back to this
@@ -28,15 +32,23 @@ window.addEventListener("load", function() {
         if (takeoffFlag){
             flightStatus.innerHTML = "Shuttle in flight.";
             shuttleBackground.style.backgroundColor = 'blue';
-            spaceShuttleHeight.innerHTML = '10,000';
+            spaceShuttleHeight.innerHTML = '10000';
+            rocket.style.bottom = `${parseInt(rocket.style.bottom)}px`
+            rocketInFlight = true;
         }
     });
 
     landing.addEventListener("click", function () {
+        if(!rocketInFlight) {
+            window.alert("Rocket is not in flight.");
+        } else {
         window.alert("The shuttle is landing. Landing gear engaged.");
         flightStatus.innerHTML = "The shuttle has landed.";
         shuttleBackground.style.backgroundColor = 'green';
         spaceShuttleHeight.innerHTML = '0';
+        rocket.style.bottom = "0px";
+        rocketInFlight = false;
+        }
 
     });
 
@@ -46,31 +58,54 @@ window.addEventListener("load", function() {
             flightStatus.innerHTML = "Mission aborted.";
             shuttleBackground.style.backgroundColor = 'green';
             spaceShuttleHeight.innerHTML = '0';
+            rocket.style.bottom = "0px";
+            rocket.style.left = "0px";
+            rocketInFlight = false;
         }
     })
 
     up.addEventListener("click", function() {
+        if(!rocketInFlight){
+            window.alert("Launch Rocket to maneuver.")
+        } else {
         let rocketTopNum = parseInt(rocket.style.bottom) + 10;
         rocket.style.bottom = `${rocketTopNum}px`
         console.log(rocket.style.bottom)
+        spaceShuttleHeightNum = parseInt(spaceShuttleHeight.innerHTML) + 10000;
+        spaceShuttleHeight.innerHTML = spaceShuttleHeightNum;
+        }
     });
 
     down.addEventListener("click", function() {
+        if(!rocketInFlight){
+            window.alert("Launch Rocket to maneuver.")
+        } else {
         let rocketTopNum = parseInt(rocket.style.bottom) - 10;
         rocket.style.bottom = `${rocketTopNum}px`
-        console.log(rocket.style.bottom)    
+        console.log(rocket.style.bottom)
+        spaceShuttleHeightNum = parseInt(spaceShuttleHeight.innerHTML) - 10000;
+        spaceShuttleHeight.innerHTML = spaceShuttleHeightNum;   
+        }
     });
 
     left.addEventListener("click", function() {
+        if(!rocketInFlight){
+            window.alert("Launch Rocket to maneuver.")
+        } else {
         let rocketTopNum = parseInt(rocket.style.left) - 10;
         rocket.style.left = `${rocketTopNum}px`
-        console.log(rocket.style.left)    
+        console.log(rocket.style.left)
+        }    
     });
 
     right.addEventListener("click", function() {
+        if(!rocketInFlight){
+            window.alert("Launch Rocket to maneuver.")
+        } else {
         let rocketTopNum = parseInt(rocket.style.left) + 10;
         rocket.style.left = `${rocketTopNum}px`
-        console.log(rocket.style.left)    
+        console.log(rocket.style.left)   
+        } 
     });
 
         
